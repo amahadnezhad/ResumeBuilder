14ee5d25c4a68904815508a6ee85737af3e13b09
amahadnezhad <amahadnezhad@gmail.com>
2024-08-05 22:38:40 +0330

    Add CustomUser Model

--- config/settings.py
+++ config/settings.py
@@ -126,3 +126,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Accounts Config
+AUTH_USER_MODEL = 'accounts.CustomUser'
